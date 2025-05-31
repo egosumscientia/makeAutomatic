b906dc5e94a4b0fe7ea501e47ec9b950089dde55
pauloenrique <egosumscientia@gmail.com>
2025-05-31 13:44:42 -0500

    landing page

--- WebSite/makeAutomatic/website/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    nombre = forms.CharField(max_length=100, label='Nombre')
-    email = forms.EmailField(label='Correo electrónico')
-    mensaje = forms.CharField(widget=forms.Textarea, label='Mensaje')
--- WebSite/makeAutomatic/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('servicios/', views.services, name='services'),
-    path('portafolio/', views.portfolio, name='portfolio'),
-    path('contacto/', views.contact, name='contact'),
-]
